69c64ab5c4b62959eb89b4e431e520721b46d478
vsamaru <vsamaru@gmail.com>
2020-11-19 17:27:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const setCors = require('./utils/setCors')
 
 const graphQLOptions = {
   // Set the path for the GraphQL server
-  baseEndpoint: '/___graphql',
+  baseEndpoint: '/',
 
   // Set the path for the GraphQL playground
   // This option can be removed to disable the playground route
